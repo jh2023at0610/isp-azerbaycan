f7f78f223197beef3f25d2cfaa74c00702903318
ISP Developer <developer@ispazerbaycan.az>
2025-10-22 00:20:59 +0400

    Fix TypeScript errors: Remove next-intl internationalization

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-// Temporarily disable middleware to debug routing
-export default function middleware() {
-  // No-op middleware
-}
-
-export const config = {
-  matcher: []
-};
--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-const withNextIntl = require('next-intl/plugin')(
-  './i18n.ts'
-);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -17,4 +13,4 @@ const nextConfig = {
   generateEtags: false,
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
